4712edb8e8d4d1b5e0ccbe51a3e953d7000afcb9
Shivam Baghel <shivambghl2000@gmail.com>
2023-05-26 17:27:57 +0530

    All working well

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,8 +17,29 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false)
   const [loading, setLoading] = useState(false)
   
-  const handleSubmit = () => { 
-
+  const handleSubmit = async(e) => { 
+    e.preventDefault();
+    if(form.prompt && form.photo){
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8081/api/v1/post',{
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json',
+          },
+          body:JSON.stringify(form)
+        })
+        const respost = await response.json();
+        console.log("MY LOG"+respost);
+        navigate('/');
+      } catch (error) {
+        alert(error)
+      }finally{
+        setLoading(false);
+      }
+    }else{
+      alert("Please generate an image first")
+    }
   }
 
   const handleChange =(e) => { 
@@ -36,7 +57,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true)
 
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('http://localhost:8081/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -48,7 +69,7 @@ const CreatePost = () => {
         })
         
         const data = await response.json();
-        console.log(data);
+        console.log(data.photo);
         setForm({...form,photo:`data:image/jpeg;base64,${data.photo}`})
       } catch (error) {
         alert(error)
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import * as dotenv from 'dotenv'
 import cors from 'cors'
+
 import connectDB from "./mongodb/connect.js";
 import postRoutes from './routes/postRoutes.js'
 import dalleRoutes from './routes/dalleRoutes.js'
@@ -18,7 +19,7 @@ app.use("/api/v1/dalle",dalleRoutes)
 const startServer = async()=>{
     try {
         connectDB(process.env.MONGODB_URL) 
-        app.listen(8080,()=> console.log("Server has started on port http://localhost:8080"))
+        app.listen(8081,()=> console.log("Server has started on port http://localhost:8081"))
 
     } catch (error) {
         console.log(error);   
